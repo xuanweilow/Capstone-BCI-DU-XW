2a2f079a2546e952ddb2377bf96359b99026a4a7
xuanweilow <low.xwei@gmail.com>
2017-10-14 21:41:55 +1100

    Update bpfilt.m

--- bpfilt.m
+++ bpfilt.m
@@ -1,11 +1,5 @@
-function [output] = bpfilt(input,Fs,Fstart,Fend)
-    % input_c takes a matrix whose rows are a time series data & columns are variables
-
-    flt = @(f)(f>=Fstart & f<=Fend); 
-%     flt = designfilt('bandpassfir','StopbandFrequency1',Fstart-1,'PassbandFrequency1',Fstart,'PassbandFrequency2',Fend,...
-%                      'StopbandFrequency2',Fend+1,'StopbandAttenuation1',50,'PassbandRipple',1,'StopbandAttenuation2',50,...
-%                      'DesignMethod','kaiserwin','SampleRate',Fs);
-% %     fvtool(flt); for filter analyse
+function [output] = bpfilt(input,Frfilt)
+    % input takes a matrix whose rows are a time series data & columns are variables
     
     for c = 1:size(input,2)
         input_c = input(:,c);
@@ -21,10 +15,7 @@ function [output] = bpfilt(input,Fs,Fstart,Fend)
         padd = [in1;input_c;in2];        
 
         % Filtering
-        t = length(padd);
-        ft_out = fft(padd).*flt(Fs*(0:t-1)/t)';
-        outpad_c = real(ifft(ft_out));
-%         outpad_c = filtfilt(flt,padd);
+        outpad_c = filtfilt(Frfilt,padd);
 
         output(:,c) = outpad_c((length(in1)+1):(length(in1)+length(input_c))); % Removing padded values
     end
